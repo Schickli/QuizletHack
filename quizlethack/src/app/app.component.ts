74469225313dcc1cf09c7eeb76089559fe93771b
Schickli <67188361+Schickli@users.noreply.github.com>
2023-05-18 14:45:25 +0200

    share

--- quizlethack/src/app/app.component.ts
+++ quizlethack/src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'quizlethack';
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private clipboard: Clipboard,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  shareUrl() {
+    this.clipboard.copy('https://quizlet-hack.vercel.app/');
+    this.openSnackBar('Url copied. Share it with your friends! :)', 'Close');
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
 
   goHome() {
     this.router.navigate(['']);
